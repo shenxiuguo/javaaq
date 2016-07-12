f99ed33b9108967b4766d75627e897ad93745c43
shenx <shenx@DESKTOP-A4R034P>
2016-07-12 23:41:09 +0800

    解决死循环问题

--- QA/src/main/java/com/shenxiuguo/QA/javafx/tree/FileTreeItem.java
+++ QA/src/main/java/com/shenxiuguo/QA/javafx/tree/FileTreeItem.java
@@ -2,12 +2,13 @@ package com.shenxiuguo.QA.javafx.tree;
 
 import java.io.File;
 
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.TreeItem;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-public class FileTreeItem extends TreeItem<File> {
+public class FileTreeItem extends TreeItem<String> {
 	private static final Image dirIconImage = new Image(FileTreeItem.class.getResource("dir-icon.png").toString());
 	private static final Image fileIconImage = new Image(FileTreeItem.class.getResource("file-icon.png").toString());
 	
@@ -15,8 +16,11 @@ public class FileTreeItem extends TreeItem<File> {
 	
 	private boolean isFirstTimeChildren = true;
 	
+	private File file;
+	
 	public FileTreeItem(File file) {
-		super(file);
+		super(file.getName());
+		this.file = file;
 		if(file.isDirectory()) {
 			isLeaf = false;
 			setGraphic(new ImageView(dirIconImage));
@@ -32,24 +36,27 @@ public class FileTreeItem extends TreeItem<File> {
     }
 	
 	@Override 
-	public ObservableList<TreeItem<File>> getChildren() {
-		System.out.println(getValue());
+	public ObservableList<TreeItem<String>> getChildren() {
 		if(isFirstTimeChildren) {
-			ObservableList<TreeItem<File>> children = super.getChildren();
-			File file = getValue();
+			//在第一次的时候必须在改处提前设置，否则会进入死循环
+			isFirstTimeChildren = false;
+			ObservableList<TreeItem<String>> children = super.getChildren();
 			if(file.isDirectory()) {
 				File[] files = file.listFiles();
-				for (File f : files) {
-					children.add(new FileTreeItem(f));
+				if(files != null) {
+					ObservableList<TreeItem<String>> list = FXCollections.observableArrayList();
+					for (File f : files) {
+						list.add(new FileTreeItem(f));
+					}
+					children.clear();
+					children.setAll(list);
 				}
 			}
-			isFirstTimeChildren = false;
+			
+//			isFirstTimeChildren = false; //不能在这里才设置改值  children.setAll(list);会回掉getChildren方法，否则会进入死循环
 		}
 		
 		return super.getChildren();
     }
 	
-	private void justtest() {
-		File value2 = getValue();
-	}
 }
